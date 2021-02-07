8c6572ef7977aa37807cf6c004e4b961a467e400
Alejandro Barrios <w.alejandro.barrios@gmail.com>
2021-02-07 10:57:56 -0600

    Menu and init class

---
+++ App.py
@@ -0,0 +1,8 @@
+from Menu import *
+
+class App:
+
+    def __init__(self):
+        menu = Menu()
+
+app = App()
---
+++ Menu.py
@@ -0,0 +1,38 @@
+class Menu:
+
+    answer = 0
+
+    def __init__(self):
+        self.create_menu()
+
+    def create_menu(self):
+        print("Menu Principal")
+        print("Selecciona una opción a continuación")
+        print("1. Cargar archivo de entrada")
+        print("2. Desplegar listas ordenadas")
+        print("3. Despleagar búsquedas")
+        print("4. Desplegar todas")
+        print("5. Desplegar todas a archivo")
+        print("6. Salir")
+        print("> ", end='')
+        answer = input()
+        print("------------------------------------")
+        print("")
+        self.select_menu_option(answer)
+
+    def select_menu_option(self, option):
+        if option == "1":
+            print(option)
+        elif option == "2":
+            print(option)
+        elif option == "3":
+            print(option)
+        elif option == "4":
+            print(option)
+        elif option == "5":
+            print(option)
+        elif option == "6":
+            quit()
+        else:
+            print("Selecciona una opción válida")
+            self.create_menu()
